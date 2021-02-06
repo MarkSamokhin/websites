81e38f27b3310780593611e2ce0c8e7cb642b0f5
Mark PC <quaaa1oj@gmail.com>
2021-02-06 13:19:06 +0200

    header and promo section

--- gulpfile.js
+++ gulpfile.js
@@ -7,31 +7,28 @@ const cleanCSS = require('gulp-clean-css');
 
 // Static server
 gulp.task('server', function() {
-    browserSync.init({
+
+    browserSync({
         server: {
             baseDir: "src"
         }
     });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('styles', function(){
-    return gulp.src("src/sass/*.+(scss|sass)")
-            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-            .pipe(rename({
-                prefix: "bonjour-",
-                suffix: "-hola",
-              }))
-            .pipe(autoprefixer({
-                cascade: false
-            }))
-            .pipe(cleanCSS({compatibility: 'ie8'}))
-            .pipe(gulp.dest("src/css"))
-            .pipe(browserSync.stream());
+gulp.task('styles', function() {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function(){
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
-    gulp.watch("src/*.html").on("change", browserSync.reload);
-});
+gulp.task('watch', function() {
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+})
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
